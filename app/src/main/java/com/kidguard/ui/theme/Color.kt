b41cf0ae103cc6a7f9340487060bf55a941f0b15
shiyande123 <364630413@qq.com>
2026-05-25 11:09:32 +0800

    chore: upload app/src/main/java/com/kidguard/ui/theme/Color.kt

---
+++ app/src/main/java/com/kidguard/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.kidguard.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Purple
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// KidGuard Brand Colors
+val ShieldGreen = Color(0xFF4CAF50)
+val AlertOrange = Color(0xFFFF9800)
+val DangerRed = Color(0xFFF44336)
+val SafeBlue = Color(0xFF2196F3)
